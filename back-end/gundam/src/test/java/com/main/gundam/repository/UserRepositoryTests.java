608762ef8661484183c3a801767134d8cbbc150b
dchkang83 <dchkang83@naver.com>
2022-07-22 11:53:56 +0900

    user repository test case

--- back-end/gundam/src/test/java/com/main/gundam/repository/UserRepositoryTests.java
+++ back-end/gundam/src/test/java/com/main/gundam/repository/UserRepositoryTests.java
@@ -3,15 +3,10 @@ package com.main.gundam.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.util.Assert;
-
-import com.main.gundam.domain.User;
-import com.main.gundam.repository.UserRepository;
-
-import lombok.extern.slf4j.Slf4j;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import com.main.gundam.domain.User;
+import lombok.extern.slf4j.Slf4j;
 
 // @RunWith(SpringRunner.class)
 @SpringBootTest // 전체 빈을 다 등록하고, 모든 application.yml를 반영해서 테스트를 하기 때문에 통합테스트용으로 많이 사용
@@ -30,7 +25,7 @@ public class UserRepositoryTests {
 
         // when
         userRepository.save(user);
-        
+
         User retrivedUser = userRepository.findById(user.getUserNo()).get();
 
         // then
