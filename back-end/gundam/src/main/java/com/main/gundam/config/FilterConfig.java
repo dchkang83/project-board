9b7a50a52f3f55737cb36988f6b18368eb113a02
dchkang83 <dchkang83@naver.com>
2022-07-25 21:08:21 +0900

    filter test

---
+++ back-end/gundam/src/main/java/com/main/gundam/config/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.main.gundam.config;
+
+import com.main.gundam.config.filter.MyFilter1;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<MyFilter1> filter1() {
+        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+        bean.addUrlPatterns("/*");
+        bean.setOrder(0); // 낮은 번호가 필터중에서 가장 먼저 실행됨.
+        return bean;
+    }
+
+//    @Bean
+//    public FilterRegistrationBean<MyFilter2> filter2() {
+//        FilterRegistrationBean<MyFilter2> bean = new FilterRegistrationBean<>(new MyFilter2());
+//        bean.addUrlPatterns("/*");
+//        bean.setOrder(1); // 낮은 번호가 필터중에서 가장 먼저 실행됨.
+//        return bean;
+//    }
+}
