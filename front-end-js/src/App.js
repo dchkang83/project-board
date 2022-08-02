52e6a9aafecc22cc48ca48af43bebc7cf215a1a0
dchkang83 <dchkang83@naver.com>
2022-08-02 15:27:48 +0900

    react js 기본 및 paths alias 설정

---
+++ front-end-js/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from '~/views/components/Test'
+
+function App() {
+  return (    
+    <div className="App">
+      
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Test />
+      </header>
+    </div>
+  );
+}
+
+export default App;
