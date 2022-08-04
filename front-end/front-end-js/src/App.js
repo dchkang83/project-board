a43f1398c394ad80552bb6f8ae93eb4b62b5c9a6
dchkang83 <dchkang83@naver.com>
2022-08-04 13:32:41 +0900

    react dom & mui 설정

--- front-end/front-end-js/src/App.js
+++ front-end/front-end-js/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Test from '~/views/components/Test'
+import React from 'react';
+import RootRoutes from '~/routes/RootRoutes';
 
-function App() {
-  return (    
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Test />
-      </header>
-    </div>
+const App = () => {
+  return (
+    <React.Suspense>
+      <RootRoutes />
+    </React.Suspense>
   );
 }
 
